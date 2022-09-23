498800dc9484b49f2d1a1a5dd0155a04f29bae40
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-09-23 10:32:11 -0400

    commit tree d622441872056bb01548313b684df15bc5d50dad

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum:250}
+    validates :category, acceptance: {accept: ['Fiction', 'Non-Fiction']}
+
 end
